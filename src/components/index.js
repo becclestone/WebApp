5ffbcc26ec493457df9cef5f439c188e28f740cc
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-09 15:55:24 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,5 @@
 export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as About } from "./About";
-export { default as Contact } from "./Contact";
+
+
