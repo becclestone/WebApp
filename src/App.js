dea9f2ed1e9909977e8d4bf58331569feeaa3987
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-09 16:01:27 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Footer, Home, About} from "./components";
+import { Footer, Home, Viewer} from "./components";
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route path="/" exact component={() => <Home />} />
-          <Route path="/about" exact component={() => <About />} />
+          <Route path="/viewer" exact component={() => <Viewer />} />
         </Switch>
         <Footer />
       </Router>
