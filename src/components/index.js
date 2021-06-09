278b8480c4c3b78b62cd896765b22a9a246c60da
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-09 16:01:56 -0400

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 // export { default as Navigation } from "./Navigation";
 export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
-export { default as About } from "./About";
+export { default as Viewer } from "./Viewer";
 
 
