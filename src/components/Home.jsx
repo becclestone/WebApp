91e7f8fb6851e81494a39be6cf173a5ced7e27ae
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-10 14:04:51 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { Link, withRouter } from "react-router-dom";
 function Home() {
   return (
     <div className="home">
-       <a href="/.auth/login/aad?post_login_redirect_uri=https://jolly-dune-0aa7eb110.azurestaticapps.net/viewer">Login</a>
+       <a href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</a>
     </div>
   );
 }
