259e109e7e8663bcd0cc1c2775ef21a00090007f
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-10 16:37:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+      <Navigation />
         <Switch>
           <Route path="/" exact component={() => <Home />} />
           <Route path="/viewer" exact component={() => <Viewer />} />
