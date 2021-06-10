bdb9cd5322c6798fc1e8a2b241f54f439410b8a4
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-10 16:35:34 -0400

    Create Navigation.jsx

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+
+function Navigation(props) {
+  return (
+    <div className="navigation">
+      <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <div class="container">
+          <Link class="navbar-brand" to="/">
+            React Multi-Page Website
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default withRouter(Navigation);
