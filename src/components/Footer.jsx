d993e74558bdb088df1b40c0a72f20b226d208ec
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-10 16:03:33 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <footer class="py-5 bg-dark fixed-bottom">
+      <header class="py-5 bg-dark fixed-bottom">
         <div class="container">
           <p class="m-0 text-center text-white">
             Copyright &copy; Your Website 2020
           </p>
         </div>
-      </footer>
+      </header>
     </div>
   );
 }
