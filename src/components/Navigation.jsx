82224471e126be1f5bf749800b8226685fc2fb3f
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-10 17:21:31 -0400

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,7 +7,7 @@ function Navigation(props) {
     <div className="navigation">
       <nav class="navbar navbar-expand navbar-dark bg-dark">
         <div class="container">
-        <div style={{display: 'flex', justifyContent:'center', alignItems:'center', height:'100vh', color:'white'}}>
+        <div style={{display: 'flex', alignItems:'center', color:'white'}}>
           <h1>Breast Tissue Clinical Study</h1>
         </div>
         </div>
