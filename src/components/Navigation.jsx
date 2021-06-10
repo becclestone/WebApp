6e71f61063c31b1efb8b33445e24b8acbc67b00d
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-10 17:39:38 -0400

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,13 @@
 
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import Navigation.css;
 
 function Navigation(props) {
   return (
     <div className="navigation">
       <nav class="navbar navbar-expand navbar-dark bg-dark">
-        <div class="container">
-        <div style={{display: 'flex', justifiedContent:'center', color:'white'}}>
+        <div class="centerednav">
           <h1>Breast Tissue Clinical Study</h1>
         </div>
         </div>
