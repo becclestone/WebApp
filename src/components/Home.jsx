246c1f731b45f1d314be80a2f3561c9fc09b4cf6
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-06-11 10:22:36 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,8 +5,6 @@ function Home() {
   return (
     <div className="home">
        <a href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</a>
-       <p></p>
-       <a href="https://gray-dune-0f8914010.azurestaticapps.net/viewer">Try OpenSeaDragon Viewer</a>
     </div>
   );
 }
