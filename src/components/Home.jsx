9b215b7154f5f604bc414189a0538cccb5c4ddb6
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-11 11:19:51 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,9 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import './Home.css';
 
 function Home() {
   return (
-<style>
-.container {
-  height: 200px;
-  position: relative;
-  border: 3px solid green;
-}
-
-.vertical-center {
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  -ms-transform: translateY(-50%);
-  transform: translateY(-50%);
-}
-</style>
-
 <div class="container">
   <div class="vertical-center">
     <p>
