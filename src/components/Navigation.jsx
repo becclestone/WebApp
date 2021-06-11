a8060294427d95cff5d296de74b0f7c9109572dc
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-10 21:42:24 -0400

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,27 @@
 
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-import Typography from '@material-ui/core/Typography';
+import TypoGraphy from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function Navigation(props) {
   return (
-    <Typography align="center">Breast Tissue Clinical Study</Typography>} />
-  );
+   <List component="nav">
+      <ListItem compnen="div">
+        <ListItemText inset>
+          <TypoGraphy color="inherit" variant="title">
+            Breat Tissue Clinical Study
+          </TypoGraphy>
+        </ListItemText>
+      </ListItem>
+    </List>
+  )
 }
+
+Navigation.propTypes = {
+  classes: PropTypes.object.isRequired\
+};
 
 export default withRouter(Navigation);
