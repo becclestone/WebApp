0880ca330281225cc24ad855edfd64023f63f983
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-11 10:56:04 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+})); 
+
 function Home() {
+  const classes = useStyles();
   return (
-    <div className="home">
-       <a href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</a>
-        <p>Try to open this <a href="viewer">View OpenseaDragon</a> </p>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseLine />
+        <div className="classes.paper">
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+          Sign in
+          </Typography>
+          <a href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</a>
+          <p>Try to open this <a href="viewer">View OpenSeaDragon</a> </p>
+        </div>
+    </Container>
   );
 }
 
