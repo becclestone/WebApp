8fa9a2555638f446df0a2a00b5f77d352ce0ef70
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-11 11:14:54 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,43 +1,33 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-import CssBaseLine from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-})); 
 
 function Home() {
-  const classes = useStyles();
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseLine />
-        <div className="classes.paper">
-          <Typography component="h1" variant="h5">
-          Sign in
-          </Typography>
-          <a href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</a>
+<style>
+.container {
+  height: 200px;
+  position: relative;
+  border: 3px solid green;
+}
+
+.vertical-center {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  -ms-transform: translateY(-50%);
+  transform: translateY(-50%);
+}
+</style>
+
+<div class="container">
+  <div class="vertical-center">
+    <p>
+    <a href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</a>
           <p>Try to open this <a href="viewer">View OpenSeaDragon</a> </p>
-        </div>
-    </Container>
+    </p>
+  </div>
+</div
+
   );
 }
 
