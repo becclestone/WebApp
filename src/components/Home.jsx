454cba7cea353eac79ff4a67245c9b970c033c2a
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-14 15:51:51 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 import './Home.css';
-import { Button, Card } from "@material-ui/core";
+import { Button, Box } from "@material-ui/core";
 
 function Home() {
   return (
+    <Box pt={3}
     <div class ="container">
     <div class="center">
     <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</Button>
           <p>Try to open this <a href="viewer">View OpenSeaDragon</a></p>
     </div>
     </div>
+</Box>
   );
 }
 
