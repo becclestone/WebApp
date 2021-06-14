0fd58e759e8a5ac811bf2a0e6b26ee9b745527af
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-06-14 10:24:16 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -20,7 +20,7 @@ function Viewer() {
   }, []);
 
   const getImages = async () => {
-    const response = await fetch("/api/deepzoom/pictures3.json")
+    const response = await fetch("https://webappimages.blob.core.windows.net/dzi/pictures3.json")
     let image = await response.json();
     console.log('image', image)
     setImages(image.groups)
