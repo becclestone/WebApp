030e7653660e448a92ef9f65b32633cd475a2402
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-14 15:39:58 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,10 @@ import { Button, Card } from "@material-ui/core";
 
 function Home() {
   return (
-    <div className="home">
     <div class ="container">
     <div class="center">
     <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</Button>
           <p>Try to open this <a href="viewer">View OpenSeaDragon</a></p>
-    </div>
     </div>
     </div>
   );
