4eb5732af40bcd104c6495844ab1add51de0d7de
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-14 15:55:15 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Button, Box } from "@material-ui/core";
 
 function Home() {
   return (
-    <Box pt={3}
+    <Box pt={3}>
     <div class ="container">
     <div class="center">
     <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</Button>
