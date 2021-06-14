82ae5ffb67d59ca1cfe3f5eafb1200c2db6d8b00
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-14 11:12:08 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -82,7 +82,7 @@ function Viewer() {
       </div>
       <div>  
           <Typography align="right">
-            User: {'\u00A0'}<b><div id="user"  ></div> </b>
+            User:<b><div id="user"  ></div> </b>
             <span id='consolelog'></span>
             </Typography>
           <Box m={3} pt={3}>
