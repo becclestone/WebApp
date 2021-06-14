dbc3e4e865d8bc9177d4c1827d3ccd47364a6d9e
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-06-14 12:18:22 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -47,26 +47,29 @@ function Viewer() {
         console.log(clientPrincipal);
       }
 
-      const Button = styled.button
-          background-color: black;
-          color: white;
-          font-size: 20px;
-          padding: 10px 60px;
-          border-radius: 5px;
-          margin: 10px 0px;
-          cursor: pointer;
-          &:disabled {
-            color: grey;
-            opacity: 0.7;
-            cursor: default;
-          };
+      const Button = styled.button`
+        background-color: black;
+        color: white;
+        font-size: 20px;
+        padding: 10px 60px;
+        border-radius: 5px;
+        margin: 10px 0px;
+        cursor: pointer;
+        &:disabled {
+          color: grey;
+          opacity: 0.7;
+          cursor: default;
+        }
+      `;
 
-      const ButtonToggle = styled(Button)
+      const ButtonToggle = styled(Button)`
         opacity: 0.6;
         ${({ active }) =>
           active &&
+          `
           opacity: 1;
-        };
+        `}
+      `;
 
 
   return (
@@ -92,7 +95,7 @@ function Viewer() {
                       <ButtonToggle
                         key={index}
                         active={active === index}
-                        onClick={() => { setActive(type);
+                        onClick={() => { setActive(index);
                           return previewImage(slide);
                         }}
                       >
