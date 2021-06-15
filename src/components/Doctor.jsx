abe7334dbdddb7f699b4802f35b7b68a5a0fa3f4
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-15 14:41:38 -0400

    Update Doctor.jsx

--- src/components/Doctor.jsx
+++ src/components/Doctor.jsx
@@ -1,1 +1,22 @@
 
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import './Home.css';
+import { Button, Box } from "@material-ui/core";
+
+function Doctor() {
+  return (
+    <Box pt={4}>
+    <div class ="container">
+    <div class="center">
+      <p> [INSERT TEXT HERE] </p>
+      <p> </p>
+        <p>
+    <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Doctor A</Button>
+    </div>
+    </div>
+</Box>
+  );
+}
+
+export default Doctor;
