317338b025df274cf0472c79ddd2b3148a6df1dc
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-06-15 17:57:10 -0400

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,6 +7,8 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     alignItems: 'center',
     flexGrow: 1
   },
