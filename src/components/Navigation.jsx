290681b28acb99717e16be2d906b910a86975941
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-06-15 16:55:30 -0400

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,11 +3,10 @@ import './App.css';
 import { Link, withRouter } from "react-router-dom";
 import { AppBar, Toolbar, Typography, Container } from "@material-ui/core";
 
-
 function Navigation() {
   return (
   <div className = "headerBar">
-   <AppBar position="static" centerTitle="true" style={{backgroundColor: '#333333'}}>
+   <AppBar position="static" centerTitle="true" style={{align: 'center'}}>
     <Container maxWidth="md">
       <Toolbar>
         <Typography variant="h4" color="inherit" style={{align:"center"}}>
