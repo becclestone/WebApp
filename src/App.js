841b7de2d8fb8af240d4dafea2adcf12f393a906
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-15 14:39:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation, Home, Viewer} from "./components";
+import { Navigation, Home, Viewer, Doctor} from "./components";
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,7 @@ function App() {
         <Switch>
           <Route path="/" exact component={() => <Home />} />
           <Route path="/viewer" exact component={() => <Viewer />} />
+          <Route path="/doctor" exact component={() => <Doctor />} />
         </Switch>
       </Router>
     </div>
