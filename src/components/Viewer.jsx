385a9f4c3ee5b561fe9a3e1ec8a499a6a6d9d49a
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-06-22 17:41:31 -0400

    Update Viewer.jsx

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -125,7 +125,7 @@ export default function Viewer() {
     const response = await fetch("https://webappimages.blob.core.windows.net/dzi/pictures3.json", {
                               method: 'GET',
                               credentials: 'include', 
-                              headers: 'Access-Control-Allow-Origin': '*'}); //api/deepzoom/pictures3.json""https://miradortest.z13.web.core.windows.net/pictures3.json
+                              headers: {'Access-Control-Allow-Origin': '*'}}); //api/deepzoom/pictures3.json""https://miradortest.z13.web.core.windows.net/pictures3.json
     let image = await response.json();
     console.log('image', image)
     setImages(image.groups)
