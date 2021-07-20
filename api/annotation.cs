e237fad43725b351c5753cd93abf6ab2f167327c
smsparkes <67698554+smsparkes@users.noreply.github.com>
2021-07-20 16:08:08 -0400

    annotat_update

--- api/annotation.cs
+++ api/annotation.cs
@@ -7,29 +7,133 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Microsoft.Azure.Documents.Client;
+using System.Security.Claims;
+using System.Collections.Generic;
 
 namespace Microsoft.Function
 {
-    public static class saveAnnotation
+
+    public class AnnotationItem
+    {
+        [JsonProperty("userId")]
+        public string userId { get; set; }
+        [JsonProperty("id")]
+        public string id { get; set; }
+        //[JsonProperty("annotations")]
+        public object[] AnnotationJson { get; set; }
+    }
+    
+    public class AnnotationProps
+    {
+        [JsonProperty("type")]
+        public string type {get; set; }
+        [JsonProperty("body")]
+        public List<AnnotationBody> body {get; set; }
+        [JsonProperty("target")]
+        public AnnotationTarget target {get; set; }
+        [JsonProperty("@context")]
+        public string context {get; set; }
+        [JsonProperty("id")]
+        public string id {get; set; }
+    }
+    
+    public class AnnotationBody
+    {
+        [JsonProperty("type")]
+        public string type {get; set; }
+        [JsonProperty("value")]
+        public string value {get; set; }
+        [JsonProperty("purpose")]
+        public string purpose {get; set; }
+    }
+   
+    public class AnnotationTarget
+    {
+        [JsonProperty("source")]
+        public string source {get; set; }
+        [JsonProperty("selector")]
+        public AnnotationSelector selector {get; set; }
+    }
+    
+    public class AnnotationSelector
+    {
+        [JsonProperty("type")]
+        public string type {get; set; }
+        [JsonProperty("conformsTo")]
+        public string conformsTo {get; set; }
+        [JsonProperty("value")]
+        public string value {get; set; }
+    }
+
+    public static class Annotation
     {
         [FunctionName("saveAnnotation")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+        public static  void RunSave(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "annotation/{imageId}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "medimages",
+                collectionName: "Annotations",
+                ConnectionStringSetting = "CosmosDBConnection")
+            ] out dynamic document,
+            string imageId,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation($"C# save annotations for {imageId}");
+            document = null;
 
-            string name = req.Query["name"];
+            // Verify identity
+            ClaimsPrincipal principal = ClientPrincipal.Parse(req);
+            if (!principal.IsInRole("contributor"))
+                return;
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string userId = principal.Identity.Name;
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string requestBody = new StreamReader(req.Body).ReadToEnd();
+            var input = JsonConvert.DeserializeObject<List<AnnotationProps>>(requestBody);
 
-            return new OkObjectResult(responseMessage);
+            document = new { userId = userId, id = imageId, AnnotationJson = input }; //new object[] { requestBody } };
+        }
+
+        [FunctionName("getAnnotation")]
+        public static async Task<IActionResult> RunGet(
+           [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "annotation/{imageId}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "medimages",  
+                collectionName: "Annotations",
+                ConnectionStringSetting = "CosmosDBConnection")
+           ]  DocumentClient client,
+            string imageId,
+            ILogger log)
+        {
+            log.LogInformation($"function GetAnnotations {imageId}");
+
+            // Verify identity
+            ClaimsPrincipal principal = ClientPrincipal.Parse(req);
+            if (!principal.IsInRole("contributor"))
+              return new UnauthorizedResult();
+
+            string userId = principal.Identity.Name;
+            AnnotationItem annotations = null;
+            try
+            {
+                var response = await client.ReadDocumentAsync<AnnotationItem>(
+                    UriFactory.CreateDocumentUri("medimages", "Annotations", imageId),
+                    new RequestOptions { PartitionKey = new Microsoft.Azure.Documents.PartitionKey(userId) });
+
+                annotations = (AnnotationItem)(dynamic)response;
+                log.LogInformation($"function GetAnnotations invoked {imageId} {userId} {annotations}");
+            }
+            catch (Exception )
+            {
+                log.LogError($"Cant find Annotations entry for {userId}  Image {imageId} in cosmosdb");
+                return new NotFoundResult();
+            }
+            log.LogInformation($"Retrieved annotation {annotations.AnnotationJson}");
+            if ( annotations != null && annotations.AnnotationJson != null && annotations.AnnotationJson.Length > 0)
+              return new OkObjectResult(annotations.AnnotationJson);
+            else
+              return new NotFoundResult();
         }
     }
 }
--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -16,120 +16,24 @@ import IconButton from '@material-ui/core/IconButton';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import { OpenSeaDragonViewer } from './OpenSeaDragonViewer';
-import styled from 'styled-components';
+import { OpenSeaDragonViewer } from './OpenSeaDragonViewer'
 import PhotoIcon from '@material-ui/icons/Photo';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
-
-const drawerWidth = 200;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(0),
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(4),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-    textAlign: 'center'
-  },
-  fixedHeight: {
-    paddingTop: 4,
-    position: "absolute",
-    top: "55%",
-    left: "50%",
-    transform: "translate(-50%, -50%)"
-  },
-    paperShift: {
-    marginLeft: 100,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-}));
+import {useStyles} from './ViewerStyle.js';
 
 export default function Viewer() {
-  
+
   const [images, setImages] = useState([]);
     const [manifest, setManifest] = useState();
-    const [active, setActive] = useState();
     const [title, setTitle] = useState();
-    
+
     setUserInfo();
 
   useEffect(() => {
     getImages();
   }, []);
-
   const getImages = async () => {
     const response = await fetch("/api/profile", {
                               method: 'GET',
@@ -139,26 +43,22 @@ export default function Viewer() {
     console.log('image', image)
     setImages(image.groups)
   };
-
   const previewImage = async (slide) => {
     setManifest(slide.slide);
     setTitle(slide.name);
   };
-
     async function getUserInfo() {
         const response = await fetch('/.auth/me');
         const payload = await response.json();
         const { clientPrincipal } = payload;
         return clientPrincipal;
       }
-
       async function setUserInfo() {
         let  clientPrincipal =  await getUserInfo();
-
         document.getElementById("user").innerHTML = clientPrincipal.userDetails;
         console.log(clientPrincipal);
       }
-  
+
   const classes = useStyles();
   const [open, setOpen] = React.useState(true);
   const handleDrawerOpen = () => {
@@ -168,7 +68,7 @@ export default function Viewer() {
     setOpen(false);
   };
   const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight, open && classes.paperShift);
-  
+
   return (
     <div className={classes.root}>
       <CssBaseline />
