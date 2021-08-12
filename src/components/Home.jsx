a3f0553ce5de39e81203b127359803b603ec7cd7
t2pearce <67698415+t2pearce@users.noreply.github.com>
2021-08-12 10:12:45 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -48,7 +48,6 @@ function Home() {
         <p>
     <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://gray-dune-0f8914010.azurestaticapps.net/viewer">Login</Button>
           <p></p>
-          <p>Try to open this <a href="/viewer2">View OpenSeaDragon</a></p>
             </p>
     </Typography>
     </Paper>
