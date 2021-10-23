5fca91ecab2c44d96fc7e5a242c381ac9f66921d
becclestone <becclestone@live.com>
2021-10-23 15:29:51 -0400

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -21,7 +21,7 @@ export default function Login() {
       <p><b>Please Login to Your Microsoft Azure Account Below:</b></p>
       <p></p>
       <p>
-        <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://zealous-bush-042f51e10.azurestaticapps.net/redirectpage">Login</Button>
+        <Button variant="contained" href="/.auth/login/aad?post_login_redirect_uri=https://parsmicroscopy.com/redirectpage">Login</Button>
       </p>
     </Typography>
     </Paper>
