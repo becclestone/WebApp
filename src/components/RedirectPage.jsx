59df980aae325404cf8678af9dc2ed4a4f5faf61
becclestone <becclestone@live.com>
2021-10-26 10:21:52 -0400

    Update RedirectPage.jsx

--- src/components/RedirectPage.jsx
+++ src/components/RedirectPage.jsx
@@ -48,6 +48,10 @@ class RedirectPage extends React.Component {
                   console.log(result[i]);
                   link = "/clinicianviewer";
                   return link;
+                } else if (result[i] == 'anonymous') {
+                  console.log(result[i]);
+                  link = "/clinicianviewer";
+                  return link;
                 }
               }
             },
